9133bfa7808d96e3da89881b9994bf03ebd89b9c
OrlandoJim24 <orlandojim2023@gmail.com>
2026-04-18 12:31:58 -0400

    feat(navbar): add logo and navigation links structure to Navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,17 @@ import "./navbar.scss"
 function Navbar() {
     return (
         <nav>
-            <div className="left">Left</div>
+            <div className="left">
+                <a href="/">
+                    <img src="/logo.png" alt="logo" />   
+                    <span>LamaEstate</span>
+                </a>
+                <a href="/"></a>
+                <a href="/">Home</a>
+                <a href="/">About</a>
+                <a href="/">Contact</a>
+                <a href="/">Agents</a>
+            </div>
             <div className="right">Right</div>
         </nav>
     );
