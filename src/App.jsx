e731ceebfa1e576d8dae02ad415084ded43ee173
OrlandoJim24 <orlandojim2023@gmail.com>
2026-04-24 19:06:29 -0400

    feat: render HomePage in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 
 import Navbar from "./components/navbar/Navbar"
 import "./layout.scss"
+import HomePage from "./routes/homePage/homePage";
 
 function App() {
   return (
     <div className="layout">
-     <Navbar/></div>
+     <Navbar/>
+     <HomePage/>
+     </div>
   )
 }
  
