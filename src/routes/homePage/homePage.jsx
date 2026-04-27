f48adde44facc589486f2e5cc93affb0c19b49c5
OrlandoJim24 <orlandojim2023@gmail.com>
2026-04-27 09:05:51 -0400

    feat: add wrapper inside textContainer with right padding for improved layout spacing

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -3,7 +3,11 @@ import "./homePage.scss";
 function HomePage() {
   return (
     <div className="homePage">
-      <div className="textContainer">text</div>
+      <div className="textContainer">
+        <div className="wrapper">
+            <h1 className="title">Find Real Estate & Get Your Dream Place Find Real Estate & Get Your Dream Place</h1></div>
+        </div>
+        
       <div className="imgContainer">
         <img src="/bg.png" alt="hero" />
       </div>
