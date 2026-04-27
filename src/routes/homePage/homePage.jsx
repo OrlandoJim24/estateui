a6f837203a6490635099304b803df846f9054746
OrlandoJim24 <orlandojim2023@gmail.com>
2026-04-27 09:10:52 -0400

    feat: add hero title and description paragraph inside wrapper for homepage content structure

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -5,9 +5,11 @@ function HomePage() {
     <div className="homePage">
       <div className="textContainer">
         <div className="wrapper">
-            <h1 className="title">Find Real Estate & Get Your Dream Place Find Real Estate & Get Your Dream Place</h1></div>
-        </div>
+            <h1 className="title">Find Real Estate & Get Your Dream Place </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis dolore excepturi quos doloremque eos accusantium at maiores et, rem sint facilis ullam ducimus a architecto sequi quae aspernatur, corporis ea!</p>
         
+        </div>
+        </div>
       <div className="imgContainer">
         <img src="/bg.png" alt="hero" />
       </div>
