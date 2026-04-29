e096da8ce540e830678fb709ed6c3bbf37f783b7
OrlandoJim24 <orlandojim2023@gmail.com>
2026-04-29 15:06:15 -0400

    feat(homepage): add statistics section with experience, awards, and properties metrics

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -1,3 +1,4 @@
+import SearchBar from "../../components/navbar/searchBar/searchBar";
 import "./homePage.scss";
 
 function HomePage() {
@@ -7,7 +8,21 @@ function HomePage() {
         <div className="wrapper">
             <h1 className="title">Find Real Estate & Get Your Dream Place </h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis dolore excepturi quos doloremque eos accusantium at maiores et, rem sint facilis ullam ducimus a architecto sequi quae aspernatur, corporis ea!</p>
-        
+        <SearchBar/>
+        <div className="boxes">
+            <div className="box">
+                <h1>16+</h1>
+                <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+                <h1>200</h1>
+                <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+                <h1>2000+</h1>
+                <h2>Properties Ready</h2>
+            </div>
+        </div>
         </div>
         </div>
       <div className="imgContainer">
