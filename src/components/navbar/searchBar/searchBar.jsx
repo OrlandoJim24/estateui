dbb943cdd1d154c8f033f1a2424f51a687410b64
OrlandoJim24 <orlandojim2023@gmail.com>
2026-04-30 22:08:27 -0400

    feat(searchbar): create basic search bar component with location and price inputs

--- src/components/navbar/searchBar/searchBar.jsx
+++ src/components/navbar/searchBar/searchBar.jsx
@@ -2,7 +2,15 @@ import './searchBar.scss'
 
 function SearchBar(){
   return (
-    <div className='searchBar'>SearchBar</div>
+    <div className='searchBar'>
+        <div className="type"></div>
+        <div className="type"></div>
+        <form >
+            <input type="text" name= "location" placeholder="City Location" />
+            <input type="number" name= "minPrice" placeholder="Min Price" />
+            <input type="number" name= "maxPrice" placeholder="Max Price" />
+        </form>
+    </div>
   )
 }
 
