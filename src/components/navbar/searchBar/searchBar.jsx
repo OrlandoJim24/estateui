f29fede0fbf07220ef317d645ed659534df5eae0
OrlandoJim24 <orlandojim2023@gmail.com>
2026-05-01 18:11:44 -0400

    feat(searchbar): add input validation limits and search button with icon

--- src/components/navbar/searchBar/searchBar.jsx
+++ src/components/navbar/searchBar/searchBar.jsx
@@ -7,8 +7,12 @@ function SearchBar(){
         <div className="type"></div>
         <form >
             <input type="text" name= "location" placeholder="City Location" />
-            <input type="number" name= "minPrice" placeholder="Min Price" />
-            <input type="number" name= "maxPrice" placeholder="Max Price" />
+            <input type="number" name= "minPrice" min={0} max={10000000} placeholder="Min Price" />
+            <input type="number" name= "maxPrice" min={0} max={10000000} placeholder="Max Price" />
+        <button>
+            <img src="/search.png" alt="" />
+        </button>
+        
         </form>
     </div>
   )
