c96d918a91a8e2e06d7f5f4c809f4291497f78a2
OrlandoJim24 <orlandojim2023@gmail.com>
2026-05-01 18:16:35 -0400

    style(searchbar): add button styling for type selection section

--- src/components/navbar/searchBar/searchBar.jsx
+++ src/components/navbar/searchBar/searchBar.jsx
@@ -3,8 +3,10 @@ import './searchBar.scss'
 function SearchBar(){
   return (
     <div className='searchBar'>
-        <div className="type"></div>
-        <div className="type"></div>
+        <div className="type">
+            <button>Buy</button>
+            <button>Rent</button>
+        </div>
         <form >
             <input type="text" name= "location" placeholder="City Location" />
             <input type="number" name= "minPrice" min={0} max={10000000} placeholder="Min Price" />
